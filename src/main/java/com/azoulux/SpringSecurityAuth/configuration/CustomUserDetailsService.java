69a839d080713c2bc7fb21df67b7a13c134c1133
Arthur Azoula <arthur.azoula@epitech.eu>
2023-11-09 23:10:29 +0100

    add security to connect in db with user/admin

--- src/main/java/com/azoulux/SpringSecurityAuth/configuration/CustomUserDetailsService.java
+++ src/main/java/com/azoulux/SpringSecurityAuth/configuration/CustomUserDetailsService.java
@@ -3,16 +3,18 @@ package com.azoulux.SpringSecurityAuth.configuration;
 import com.azoulux.SpringSecurityAuth.model.DBUser;
 import com.azoulux.SpringSecurityAuth.repository.DBUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
