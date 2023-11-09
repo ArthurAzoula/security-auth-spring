29d1ec05a439a95a6c96ce714a98bb1de1c4ccb5
Arthur Azoula <arthur.azoula@epitech.eu>
2023-11-09 23:00:25 +0100

    add security, grantedAuthorization, forms admin/user

---
+++ src/main/java/com/azoulux/SpringSecurityAuth/configuration/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.azoulux.SpringSecurityAuth.configuration;
+
+import com.azoulux.SpringSecurityAuth.model.DBUser;
+import com.azoulux.SpringSecurityAuth.repository.DBUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private DBUserRepository dbUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        DBUser user = dbUserRepository.findByUsername(username);
+
+        return new User(user.getUsername(), user.getPassword(), getGrantedAuthorities(user.getRole()));
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(String role) {
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+        return authorities;
+    }
+}
